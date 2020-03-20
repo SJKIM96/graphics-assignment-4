d4f6c4ded909e29abb23537c94c2a5f913e44c04
unknown <you7046@gmail.com>
2020-03-20 13:13:26 -0400

    init

---
+++ shader.py
@@ -0,0 +1,16 @@
+class shader:
+
+    def __shadowed(self,object,I,S,objectList):
+        
+	#Complete this helper method
+
+	return False
+
+    def __init__(self,intersection,direction,camera,objectList,light):
+
+	#Complete this method
+        
+        self.__color = (0,0,0)
+
+    def getShade(self):
+        return self.__color
